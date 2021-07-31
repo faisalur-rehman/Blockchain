f1e922401a17d634db394ea524590c9d2f12ce26
faisalur-rehman <66237466+faisalur-rehman@users.noreply.github.com>
2021-07-31 20:44:54 +0500

    Added keys blockchain component

--- src/components/Keys/Blockchain/BlockchainForm.js
+++ src/components/Keys/Blockchain/BlockchainForm.js
@@ -1,76 +1,99 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
-const BlockchainForm = ({ data }) => {
+const BlockchainForm = ({
+  data,
+  peer,
+  hashValues,
+  handleSubmit,
+  handleValueChange,
+  changeColor,
+}) => {
+  const [prevValues, setPrevValues] = useState([]);
+
+  useEffect(() => {
+    setPrevValues([
+      "0000000000000000000000000000000000000000000000000000000000000000",
+      ...hashValues,
+    ]);
+    //eslint-disable-next-line
+  }, [hashValues]);
+  console.log("prev v", prevValues);
   return (
     <div>
       <section class="blockchain-section mt-4">
-        <h1>Blockchain</h1>
-        {["Peer A", "Peer B", "Peer C"].map((peer) => (
-          <>
-            <div class="container-fluid">
-              <div class="scrolling-wrapper">
-                <h2>{peer}</h2>
-                <div class="container1">
-                  {data.map((block) => (
-                    <>
-                      <div class="row d-inline">
-                        <div class="col-xs-7">
-                          <div class="form form-bg-sucess">
-                            <form class="form-horizontal">
-                              <div class="form-group row mb-3">
-                                <label class="col-sm-2 label">Block:</label>
-                                <div class="col-sm-10">
-                                  <div class="input-group">
-                                    <span class="input-group-addon">#</span>
-                                    <input
-                                      class="form-control"
-                                      type="number"
-                                      value={block.block}
-                                    />
-                                  </div>
-                                </div>
-                              </div>
-                              <div class="form-group row mb-3">
-                                <label class="col-sm-2 label">Nonce:</label>
-                                <div class="col-sm-10">
+        <>
+          <div class="container-fluid">
+            <div class="scrolling-wrapper">
+              <h2>{peer}</h2>
+              <div class="container1">
+                {data.map((block, index) => (
+                  <>
+                    <div class="row d-inline">
+                      <div class="col-xs-7">
+                        <div
+                          className={`form form-bg-sucess ${
+                            changeColor[index] ? "bg" : ""
+                          }`}
+                        >
+                          <form
+                            class="form-horizontal"
+                            onSubmit={(e) => handleSubmit(e, peer, index)}
+                          >
+                            <div class="form-group row mb-3">
+                              <label class="col-sm-2 label">Block:</label>
+                              <div class="col-sm-10">
+                                <div class="input-group">
+                                  <span class="input-group-addon">#</span>
                                   <input
                                     class="form-control"
-                                    type="text"
-                                    value={block.nonce}
+                                    type="number"
+                                    value={block.block}
                                   />
                                 </div>
                               </div>
-                              <div class="form-group row mb-3">
-                                <label class="col-sm-2 control-label">
-                                  Coinbase:
-                                </label>
-                                <div class="col-sm">
-                                  <div class="input-group">
-                                    <div class="input-group-addon">$</div>
-                                    <input
-                                      class="form-control"
-                                      id="block2chain1coinbasevalue"
-                                      type="text"
-                                      value={block.coinbasevalue}
-                                      onkeyup="updateChain(2, 1, 4);"
-                                    />
-                                    <div class="input-group-addon">-&gt;</div>
-                                    <input
-                                      class="form-control"
-                                      id="block2chain1coinbaseto"
-                                      type="text"
-                                      value={block.coinbaseto}
-                                      onkeyup="updateChain(2, 1, 4);"
-                                    />
-                                  </div>
+                            </div>
+                            <div class="form-group row mb-3">
+                              <label class="col-sm-2 label">Nonce:</label>
+                              <div class="col-sm-10">
+                                <input
+                                  class="form-control"
+                                  type="text"
+                                  value={block.nonce}
+                                />
+                              </div>
+                            </div>
+                            <div class="form-group row mb-3">
+                              <label class="col-sm-2 control-label">
+                                Coinbase:
+                              </label>
+                              <div class="col-sm">
+                                <div class="input-group">
+                                  <div class="input-group-addon">$</div>
+                                  <input
+                                    class="form-control"
+                                    id="block2chain1coinbasevalue"
+                                    type="text"
+                                    value={block.coinbasevalue}
+                                    readOnly
+                                    onkeyup="updateChain(2, 1, 4);"
+                                  />
+                                  <div class="input-group-addon">-&gt;</div>
+                                  <input
+                                    class="form-control"
+                                    id="block2chain1coinbaseto"
+                                    type="text"
+                                    value={block.coinbaseto}
+                                    readOnly
+                                    onkeyup="updateChain(2, 1, 4);"
+                                  />
                                 </div>
                               </div>
-                              <div class="form-group row mb-3">
-                                <label class="col-sm-2 control-label">
-                                  Tx:
-                                </label>
-                                <div class="col-sm">
-                                  {block.txs.map((tx) => (
+                            </div>
+                            <div class="form-group row mb-3">
+                              <label class="col-sm-2 control-label">Tx:</label>
+                              <div class="col-sm">
+                                {block.txs &&
+                                  block.txs.map((tx, i) => (
                                     <>
                                       <div class="input-group">
                                         <div class="input-group-addon">$</div>
@@ -78,6 +101,15 @@ const BlockchainForm = ({ data }) => {
                                           class="form-control"
                                           type="text"
                                           value={tx.value}
+                                          onChange={({ target }) =>
+                                            handleValueChange(
+                                              target.value,
+                                              i,
+                                              index,
+                                              peer,
+                                              "value"
+                                            )
+                                          }
                                         />
                                         <div class="input-group-addon">
                                           From:
@@ -86,6 +118,15 @@ const BlockchainForm = ({ data }) => {
                                           class="form-control"
                                           type="text"
                                           value={tx.from}
+                                          onChange={({ target }) =>
+                                            handleValueChange(
+                                              target.value,
+                                              i,
+                                              index,
+                                              peer,
+                                              "from"
+                                            )
+                                          }
                                         />
                                         <div class="input-group-addon">
                                           -&gt;
@@ -94,6 +135,15 @@ const BlockchainForm = ({ data }) => {
                                           class="form-control"
                                           type="text"
                                           value={tx.to}
+                                          onChange={({ target }) =>
+                                            handleValueChange(
+                                              target.value,
+                                              i,
+                                              index,
+                                              peer,
+                                              "to"
+                                            )
+                                          }
                                         />
                                       </div>
                                       <div class="input-group">
@@ -106,6 +156,7 @@ const BlockchainForm = ({ data }) => {
                                           type="text"
                                           value={tx.seq}
                                           onkeyup="updateChain(2, 1, 4);"
+                                          readOnly
                                         />
                                         <div class="input-group-addon">
                                           Sig:
@@ -115,61 +166,61 @@ const BlockchainForm = ({ data }) => {
                                           id="block2chain1tx1sig"
                                           type="text"
                                           value={tx.sig}
+                                          readOnly
                                           onkeyup="updateChain(2, 1, 4);"
                                         />
                                       </div>
                                       <br />
                                     </>
                                   ))}
-                                </div>
                               </div>
-                              <div class="form-group row mb-3">
-                                <label class="col-sm-2 label">Prev:</label>
-                                <div class="col-sm-10">
-                                  <input
-                                    class="form-control"
-                                    type="text"
-                                    disabled=""
-                                    value={block.previous}
-                                  />
-                                </div>
+                            </div>
+                            <div class="form-group row mb-3">
+                              <label class="col-sm-2 label">Prev:</label>
+                              <div class="col-sm-10">
+                                <input
+                                  class="form-control"
+                                  type="text"
+                                  disabled=""
+                                  value={prevValues[index]}
+                                />
                               </div>
-                              <div class="form-group row mb-3">
-                                <label class="col-sm-2 label">Hash:</label>
-                                <div class="col-sm-10">
-                                  <input
-                                    class="form-control"
-                                    type="text"
-                                    disabled=""
-                                    value="09876dshbn876w2tfvw76t"
-                                  />
-                                </div>
+                            </div>
+                            <div class="form-group row mb-3">
+                              <label class="col-sm-2 label">Hash:</label>
+                              <div class="col-sm-10">
+                                <input
+                                  class="form-control"
+                                  type="text"
+                                  disabled=""
+                                  value={hashValues[index]}
+                                />
                               </div>
-                              <div class="form-group row mb-3">
-                                <div class="col-sm-2">
-                                  <i class="icon-spinner icon-spin icon-large"></i>
-                                </div>
-                                <div class="col-sm-10">
-                                  <button
-                                    class="btn btn-primary ladda-button"
-                                    id="block1chain1mineButton"
-                                    data-style="expand-right"
-                                  >
-                                    <span class="ladda-label">Mine</span>
-                                  </button>
-                                </div>
+                            </div>
+                            <div class="form-group row mb-3">
+                              <div class="col-sm-2">
+                                <i class="icon-spinner icon-spin icon-large"></i>
                               </div>
-                            </form>
-                          </div>
+                              <div class="col-sm-10">
+                                <button
+                                  class="btn btn-primary ladda-button"
+                                  id="block1chain1mineButton"
+                                  data-style="expand-right"
+                                >
+                                  <span class="ladda-label">Mine</span>
+                                </button>
+                              </div>
+                            </div>
+                          </form>
                         </div>
                       </div>
-                    </>
-                  ))}
-                </div>
+                    </div>
+                  </>
+                ))}
               </div>
             </div>
-          </>
-        ))}
+          </div>
+        </>
       </section>
     </div>
   );
--- src/components/Keys/Blockchain/KeyBlockchain.js
+++ src/components/Keys/Blockchain/KeyBlockchain.js
@@ -1,22 +1,190 @@
 import React, { useEffect, useState } from "react";
+import { getTokens, postBlock } from "../../../api/blockchain-api";
 import { getKeyBlockchain } from "../../../api/keys-api";
+import { useCrypto } from "../../../hooks/useCrypto";
 import BlockchainForm from "./BlockchainForm";
 
 const KeyBlockchain = () => {
-  const [data, setData] = useState([]);
+  const [calcHash] = useCrypto();
+  const [dataA, setDataA] = useState([]);
+  const [dataB, setDataB] = useState([]);
+  const [dataC, setDataC] = useState([]);
+  const [changeColorA, setchangeColorA] = useState([
+    false,
+    false,
+    false,
+    false,
+    false,
+  ]);
+  const [changeColorB, setchangeColorB] = useState([
+    false,
+    false,
+    false,
+    false,
+    false,
+  ]);
+  const [changeColorC, setchangeColorC] = useState([
+    false,
+    false,
+    false,
+    false,
+    false,
+  ]);
+  const [hashValuesA, setHashValuesA] = useState([]);
+  const [hashValuesB, setHashValuesB] = useState([]);
+  const [hashValuesC, setHashValuesC] = useState([]);
+
   useEffect(() => {
-    async function fetchBlockData() {
+    async function fetchData() {
       const {
         data: { data },
       } = await getKeyBlockchain();
-      setData(data);
+      data.map((item) => setHashValuesA((prev) => [...prev, item.hash]));
+      data.map((item) => setHashValuesB((prev) => [...prev, item.hash]));
+      data.map((item) => setHashValuesC((prev) => [...prev, item.hash]));
+      setDataA(data);
+      setDataB(data);
+      setDataC(data);
+      console.log("data", data);
     }
-    fetchBlockData();
+    fetchData();
     //eslint-disable-next-line
   }, []);
 
-  console.log("data", data);
-  return <div>{data.length > 0 && <BlockchainForm data={data} />}</div>;
+  function handleValueChange(value, txIndex, blockIndex, peer, type) {
+    let newData;
+    let newArr;
+    let newChangeArr;
+    console.log("check", value, dataB[blockIndex].txs[txIndex].value);
+    if (peer === "PeerA") {
+      newData = [...dataA];
+      newChangeArr = changeColorA;
+    } else if (peer === "PeerB") {
+      newData = [...dataB];
+      newChangeArr = changeColorB;
+    } else {
+      newData = [...dataC];
+      newChangeArr = changeColorC;
+    }
+
+    if (value === dataA[blockIndex].txs[txIndex].value) {
+      setchangeColorA([false, false, false, false, false]);
+    } else {
+      for (let i = blockIndex; i < 5; i++) {
+        newChangeArr[i] = true;
+      }
+    }
+    newData[blockIndex].txs[txIndex][type] = value;
+    newArr = [...hashValuesA];
+    newArr[blockIndex] = calcHash(value);
+
+    if (peer === "PeerA") {
+      setHashValuesA([...newArr]);
+      setDataA([...newData]);
+      setchangeColorA([...newChangeArr]);
+    } else if (peer === "PeerB") {
+      setHashValuesB([...newArr]);
+      setDataB([...newData]);
+      setchangeColorB([...newChangeArr]);
+    } else {
+      setHashValuesC([...newArr]);
+      setDataC([...newData]);
+      setchangeColorC([...newChangeArr]);
+    }
+  }
+  function handleFromChange(value, index) {}
+  function handleToChange(value, index) {}
+
+  async function handleSubmit(e, peer, index) {
+    e.preventDefault();
+    let newHash;
+    let newArr, changeArr;
+    if (peer === "PeerA") {
+      newHash = hashValuesA;
+      newArr = [...dataA];
+      changeArr = changeColorA;
+    } else if (peer === "PeerB") {
+      newHash = hashValuesB;
+      newArr = [...dataB];
+      changeArr = changeColorB;
+    } else if (peer === "PeerC") {
+      newHash = hashValuesC;
+      newArr = [...dataC];
+      changeArr = changeColorC;
+    }
+    try {
+      const {
+        data: { data },
+      } = await postBlock(newArr[index]);
+      newArr[index] = data;
+      newHash[index] = data.hash;
+      changeArr[index] = false;
+      if (peer === "PeerA") {
+        setHashValuesA([...newHash]);
+        setDataA([...newArr]);
+        setchangeColorA([...changeArr]);
+      } else if (peer === "PeerB") {
+        setHashValuesB([...newHash]);
+        setDataB([...newArr]);
+        setchangeColorB([...changeArr]);
+      } else {
+        setHashValuesC([...newHash]);
+        setDataC([...newArr]);
+        setchangeColorC([...changeArr]);
+      }
+    } catch (error) {
+      console.log(error.response);
+    }
+  }
+
+  return (
+    <>
+      <h1>Blockchain</h1>
+
+      <div>
+        {dataA.length > 0 && (
+          <BlockchainForm
+            data={dataA}
+            peer="PeerA"
+            changeColor={changeColorA}
+            handleToChange={handleToChange}
+            handleFromChange={handleFromChange}
+            handleValueChange={handleValueChange}
+            handleSubmit={handleSubmit}
+            hashValues={hashValuesA}
+          />
+        )}
+      </div>
+      <div>
+        {dataB.length > 0 && (
+          <BlockchainForm
+            data={dataB}
+            peer="PeerB"
+            changeColor={changeColorB}
+            handleToChange={handleToChange}
+            handleFromChange={handleFromChange}
+            handleValueChange={handleValueChange}
+            handleSubmit={handleSubmit}
+            hashValues={hashValuesB}
+          />
+        )}
+      </div>
+      <div>
+        {dataC.length > 0 && (
+          <BlockchainForm
+            data={dataC}
+            peer="PeerC"
+            changeColor={changeColorC}
+            handleToChange={handleToChange}
+            handleFromChange={handleFromChange}
+            handleValueChange={handleValueChange}
+            handleSubmit={handleSubmit}
+            hashValues={hashValuesC}
+          />
+        )}
+      </div>
+    </>
+  );
 };
 
 export default KeyBlockchain;
